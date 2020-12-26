74d7d1f2d855e73c0e61a653c7c8b941010274f1
Luna <yw3264@nyu.edu>
2020-12-27 01:37:14 +0800

    Terrain with stone and small plants

---
+++ Assets/Luna/Light.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Light : MonoBehaviour
+{
+
+    public float speed = 0.5f;
+    public Vector3 pointA;
+    public Vector3 pointB;
+
+    void Update()
+    {
+        float time = Mathf.PingPong(Time.time * speed, 1);
+        transform.eulerAngles = Vector3.Lerp(pointA, pointB, time);
+
+    }
+}
